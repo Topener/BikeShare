3220aef4d33d8adafb78cf95ba18eba8ca1c4b64
Rene Pot <rene@topener.nl>
2019-10-18 13:31:18 +0200

    add drawer

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,8 @@
-function doClick(e) {
-	alert($.label.text);
-}
 
-$.index.open();
+/**
+ * We're creating a new controller straight away in index.js
+ * so in case we ever want to add a login flow we can add a check
+ * here to choose which controller we want to open first. 
+ */
+
+Alloy.createController('main').getView().open();
